d77d5f067a52b778bab2865abf90127609ea1094
Pippah Gift <Pippah.gift@gmail.com>
2023-02-27 03:28:04 -0800

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
+/*the function prints out this*/
+
 int main(void)
 {
- /*the function prints out what is written*/
-
 	char string[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string);
