002cb77191d4f2f558cdbbea562a36f5e0cd3e5a
Pippah Gift <Pippah.gift@gmail.com>
2023-03-03 12:54:24 -0800

    alx-low_level_programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -24,9 +24,13 @@ int main(void)
 	else if	("n==0") {
 		printf("%d is zero\n",n); 
 	}
-	else {
+	else if ("n<0") { 
 		printf("%d is negative\n",n);
 	}
+	else{
+            
+	}
+
 	
 		
 	return (0);
