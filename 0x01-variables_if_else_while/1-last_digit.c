1840307d3d09d93a8064a1c627c2555f8118083c
Pippah Gift <Pippah.gift@gmail.com>
2023-03-04 11:44:11 -0800

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/** 
+ * what happens if i do this instead
+ *
+ * will it work or is it just the hand work of the checker
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int last;
+
+	last = n % 10;
+	if (n > 5) 
+	printf("Last digit of %d is %d and is greater than 5\n", n, last);
+       	 if (n = 0)
+	printf("Last digit of %d is %d and is 0\n", n, last);
+	else if (n <6 && n != 0)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);	
+	return (0);
+}
