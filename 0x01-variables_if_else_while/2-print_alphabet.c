f8d58a36c8c642d5c88b7af1ab610a2df78af28f
Pippah Gift <Pippah.gift@gmail.com>
2023-03-04 14:27:27 -0800

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letters = 'a';
+	for (letters = 'a'; letters <= 'z' ; ++letters)
+		putchar(letters);
+	return (0);
+}
