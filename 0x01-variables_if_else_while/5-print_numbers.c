759f4af2e2ae067c9c9e7d87114d322ddd5a6d69
Pippah Gift <Pippah.gift@gmail.com>
2023-03-04 23:30:05 -0800

    alx

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,8 @@ int main(void)
 {
 	int number;
 	number = 0;
-	for(number=0; number <= 9; number++);
-	printf("%d", number);
-	printf("\n");
+	for(number=0; number <= 9; ++number);
+	printf("%d\n", number);
 	return(0);
 }
 
