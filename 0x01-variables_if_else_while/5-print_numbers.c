cf0764f0e53fe984939f3f5da6c22f278c3ccbe2
Pippah Gift <Pippah.gift@gmail.com>
2023-03-04 23:19:43 -0800

    alx

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int number;
 	number = 0;
 	for(number=0; number <= 9; number++);
+	printf("%d", number);
 	printf("\n");
 	return(0);
 }
