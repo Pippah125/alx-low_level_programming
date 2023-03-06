a045bb102d7915d08813cbbbbf1deeec810a210a
Pippah Gift <Pippah.gift@gmail.com>
2023-03-06 12:57:26 -0800

    alx

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,11 +18,10 @@ int main(void)
         int FLOAT;
         FLOAT=sizeof(float);
         printf("Size of a char: %d bytes(s)\n", Char );
-        printf("Size of an int : %d bytes(s)\n", INT);
+        printf("Size of an int : %d bytes(s)\n",INT);
         printf("Size of a long int: %d bytes(s)\n",LongInt);
 	printf("Size of a long long int: %d bytes(s)\n",LLInt);
-	printf("Size of a long long int: %d bytes(s)\n", FLOAT);
-
+	printf("Size of a long long int: %d bytes(s)\n",FLOAT);
         return(0);
 }
 
