c6c53e368dc99eefb53c78c48bf1eda77b8cc7de
Pippah Gift <Pippah.gift@gmail.com>
2023-03-07 01:08:42 -0800

    alx

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int a;
+	int b;
+	for(a = '0'; a <= '9'; a++)
+	{
+		for(b = '0'; b <= '9'; b++)
+		{
+			if(a < b) 
+			{	
+				putchar(a);
+			putchar(b);
+			
+			if(a != '8'||(a == '8' && b != '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	}
+	return(0);
+}
