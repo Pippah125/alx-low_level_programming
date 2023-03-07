f5f85a7cda3ca3fc62931cd58243010dcf1df038
Pippah Gift <Pippah.gift@gmail.com>
2023-03-07 00:04:16 -0800

    alx

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,10 +3,10 @@ int main (void)
 {
 	int x;
 	char y;
-	for(x=0; x<=9; x++){
-		putchar(x);}
-	for(y = 'a'; y <= 'f'; y++){
-		putchar(y);}
+	for(x=0; x<=9; x++)
+		putchar(x);
+	for(y = 'a'; y <= 'f'; y++)
+		putchar(y);
 	putchar('\n');
 	return(0);
 }
