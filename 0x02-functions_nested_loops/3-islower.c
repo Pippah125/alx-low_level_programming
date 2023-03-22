519c4b71e580d93e0b2af27314b95920f7eab45a
Pippah Gift <Pippah.gift@gmail.com>
2023-03-22 10:34:14 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,14 @@
 #include "main.h"
+/**
+ * function is lower to indicate if an alphabet is lower
+ *
+ * 
+ */
 int _islower(int c)
 {
 
-	if( c >= 'a' && c <= 'z')
-	return(1);
-	else 
-	return(0);
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
 }
