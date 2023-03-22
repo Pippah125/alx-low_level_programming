a28667f8d7388f7041cefdd76efeba16521e57fd
Pippah Gift <Pippah.gift@gmail.com>
2023-03-22 09:03:47 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,11 @@
-#include<unistd.h>
 #include "main.h"
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 void print_alphabet(void)
-void print_alphabet(void);
 {
-        char b;
-        for(b = 'a'; b <= 'z'; b++)
-	{
-             _putchar(b);
-	}
+	char b;
+	for(b = 'a'; b <= 'z'; b++)
+	_putchar(b);
+
+	_putchar('\n');
 }
+
 
