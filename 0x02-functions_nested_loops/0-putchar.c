bd9ff3809faca2f58edbfaa7f959dde73acc84e7
Pippah Gift <Pippah.gift@gmail.com>
2023-03-22 04:42:31 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,12 @@
 #include<unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	write(1,"_putchar\n",13);
+	write( 1, "_putchar\n", 9 );
 
 	return(0);
 }
