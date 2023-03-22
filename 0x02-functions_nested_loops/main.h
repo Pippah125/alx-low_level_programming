9c6664018bbbf7c223e7d7bd1b6c4b5a2dfda258
Pippah Gift <Pippah.gift@gmail.com>
2023-03-22 10:41:34 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,13 @@
 void print_alphabet(void);
 int _putchar(char c);
 int _islower(int c);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 #endif
