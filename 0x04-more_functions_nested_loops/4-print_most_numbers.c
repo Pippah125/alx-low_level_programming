92d61c3834155ec11d33c35d44e22a5f7c305890
Pippah Gift <Pippah.gift@gmail.com>
2023-03-24 15:50:31 -0700

    alx-low_level_programming

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,10 +3,15 @@ void print_most_numbers(void)
 {
 	int x;
 	for (x = 0; x<= 9; x++)
-	{	if ((x == 2)|| (x == 4))
+	{	if (x == 2)|| (x == 4)
+		{
 		continue;
+		}
 		else
-		_putchar(x +'0');
-		_putchar('\n');
+		{
+			_putchar(x +'0');
+		}
+		
 	}
+	_putchar('\n')
 }
