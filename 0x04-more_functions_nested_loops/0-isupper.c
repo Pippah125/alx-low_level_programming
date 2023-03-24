2a3408b63d8b7af7ccc5ca4a6f8ab86b9d23e62e
Pippah Gift <Pippah.gift@gmail.com>
2023-03-24 02:19:21 -0700

    alx-low_level_programming

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,15 @@
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 #include "main.h"
 /**
  * _isupper-checks if alphabet is Uppercase
  * return 1 if true 
  * return 0 if false
  */
+
 int _isupper(int c)
 {
 	if(c >= 'A' && c<= 'Z')
