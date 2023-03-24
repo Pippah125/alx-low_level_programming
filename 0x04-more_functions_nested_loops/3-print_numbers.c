5843e6a66fd02b4b85fa7929e3df623bba3cf0f9
Pippah Gift <Pippah.gift@gmail.com>
2023-03-24 14:32:27 -0700

    alx-low_level_programming

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 void print_numbers(void)
 {
-	char x;
-	for (x = '0'; x <= '9'; x++);
-	{ _putchar(x + 0);
+	int  x;
+	for (x = '0'; x <= '9'; x++)
+	{ _putchar(x + '0');
 	}
        _putchar('\n');
 }       
