851f01d302b7fe1336b774ad7f67b20a904b3fae
Pippah Gift <Pippah.gift@gmail.com>
2023-03-24 01:07:09 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
 int print_sign(int n)
 {
-       	if(n > 0)
+       	if (n > 0)
 	{_putchar( '1');
 	return (1);}
-	else if(n = 0)
+	else if (n = 0)
 	{_putchar('0');
 	return (0);}
 	else
