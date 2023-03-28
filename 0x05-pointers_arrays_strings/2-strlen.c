138627df8c692d5ea9c2a43d9be93ae24d64368a
Pippah Gift <Pippah.gift@gmail.com>
2023-03-28 08:01:58 -0700

    alx-low_level_programming

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,6 @@
+#include<string.h>
+#include "main.h"
+int _strlen(char *s)
+{
+	strlen(s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef main_h
 #define main_h
+int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 #endif
