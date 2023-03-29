1b2f4568516c0b7e707d1d6f98ab371435519d4f
Pippah Gift <Pippah.gift@gmail.com>
2023-03-29 08:25:40 -0700

    alx-low_level_programming

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,9 @@
 /**
  * _strlen(char *s)- show length of string
  * @_strlen(char *s): show length of string
- * @*s: the string to be increased
+ * @s: the string to be increased
+ *
+ * Return: Always 0.
  */
 int _strlen(char *s)
 {
