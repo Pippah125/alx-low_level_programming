b512877dacae67ad5308072faf22a685029c25d0
Pippah Gift <Pippah.gift@gmail.com>
2023-03-29 06:43:55 -0700

    alx-low_level_proramming

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * void reset_to_98(int *n) - resets n to 98
- *
- *Return: Always 0 (success)
-  */
+* void reset_to_98(int *n) - resets n to 98
+*
+*Return: Always 0 (success)
+*/
 void reset_to_98(int *n)
 
 {
