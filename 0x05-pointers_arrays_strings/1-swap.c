438c24cabc1858d7984105865ba04d7437b7f1b0
Pippah Gift <Pippah.gift@gmail.com>
2023-03-29 08:16:16 -0700

    alx-low_level_programming

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* swap - swap the value of a to be
-* @swap_int(int *a, int *b): swaps the value of a to b
-* @a: will be changed to b
-* @b: will be chaned to a
+*swap - swap the value of a to be
+*@swap_int(int *a, int *b): swaps the value of a to b
+*@a: will be changed to b
+*@b: will be chaned to a
 *
-* Return: Always 0 (success)
+*Return: Always 0 (success)
 */
 void swap_int(int *a, int *b)
 {
