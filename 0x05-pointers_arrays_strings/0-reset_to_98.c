fe4ceebd94374f759452ce7283aacd9d443813ae
Pippah Gift <Pippah.gift@gmail.com>
2023-03-29 06:25:59 -0700

    alx-low_level_programming

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,8 +5,15 @@
  * @n: pointer to n
  *
  * Return: Always 0 (success)
- */
+  */
 void reset_to_98(int *n)
+	/**
+ * void reset_to_98(int *n) - resets n to 98
+ * @n: pointer to n
+ *
+ * Return: Always 0 (success)
+ */
+
 {
 	*n = 98;
 }
