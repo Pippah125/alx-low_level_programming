6ad067367cddfc54c4a80a612c645b21b824da2d
Pippah Gift <Pippah.gift@gmail.com>
2023-03-29 08:09:03 -0700

    alx-low_level_programming

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,9 @@
 
 /**
 * swap - swap the value of a to be
-*
+* @swap: swap the value of a to b
+* @a: will be changed to b
+* @b: will be chaned to a
 * Return: Always 0 (success)
 */
 void swap_int(int *a, int *b)
