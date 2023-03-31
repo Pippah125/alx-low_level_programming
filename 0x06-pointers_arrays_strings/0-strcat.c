5d25c9b04c31a551a958a3338cc7a69df27f198b
Pippah Gift <Pippah.gift@gmail.com>
2023-03-30 18:06:42 -0700

    alx-low_level_programming

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,12 @@ char *_strcat(char *dest, char *src)
 {
 	int A;
 	int B;
-	A = '0';
+	A = 0;
 	while(dest[A] !='\0')
 	{
 		A++;
 	}
-	B = '0';
+	B = 0;
 	while (src[B] !='\0')
 	{
 		dest[A] = src[B];
