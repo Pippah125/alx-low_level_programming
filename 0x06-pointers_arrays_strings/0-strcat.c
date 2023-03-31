162d9c4a941d1e24d48667cc6942bf5afa6425c9
Pippah Gift <Pippah.gift@gmail.com>
2023-03-30 17:48:10 -0700

    alx-low_level_programming

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,27 @@
 #include "main.h"
+
+/**
+ * _strcat - copies string A to string B
+ * @dest: The string to be copied
+ * @src: The string to be added to
+ *
+ */
 char *_strcat(char *dest, char *src)
 {
-	char *rdest;
-	*rdest = _putchar(dest, src)
-		return (*rdest)
+	int A;
+	int B;
+	A = 0;
+	while(dest[A] !='\0')
+	{
+		A++;
+	}
+	B = 0;
+	while (src[B] !='\0')
+	{
+		dest[A] = src[B];
+		A++;
+		B++;
+	}
+	dest[A] ='\0';
+		return (dest);
 }
