6cb56a278d2034c46340e2163a26eadf652ea9cd
Pippah Gift <Pippah.gift@gmail.com>
2023-03-30 19:00:39 -0700

    alx-low_level_programming

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,23 +5,25 @@
  * @n: the number of bytes
  * @src: The string to be added
  *
+ * Return: void
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int A;
 	int B;
+
 	A = 0;
 	while (dest[A] != '\0')
-	{ 
+	{
 		A++;
 	}
-	B= 0;
-	while (B < n && src[B] != '\0')
+	B = 0;
+	while (n > B && src[B] != '\0')
 	{
 		dest[A] = src[B];
 			A++;
 			B++;
 	}
 	dest[A] = '\0';
-		return(dest);
+		return (dest);
 }
