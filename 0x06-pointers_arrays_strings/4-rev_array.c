d6b39d9c04cda36c5a273934f8fbccf24283e469
Pippah Gift <Pippah.gift@gmail.com>
2023-03-30 19:48:22 -0700

    alx-low_level_programming

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _reverse_array  - reverses the string
+ * reverse_array  - reverses the string
  * @a: reverse string
  * @n: reverse string
  *
@@ -11,7 +11,8 @@ void reverse_array(int *a, int n)
 {
 	int A;
 	int B;
-	for ( A = 0; A < n--; A++)
+
+	for (A = 0; n-- < A; A++)
 	{
 		B = a[A];
 		a[A] = a[n];
