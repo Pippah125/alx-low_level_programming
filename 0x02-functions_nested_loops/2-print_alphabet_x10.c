ecb3d980bc36304b13cf5bb5fa19f473058d255a
Pippah Gift <Pippah.gift@gmail.com>
2023-04-04 23:03:51 -0700

    alx-low

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,11 @@ void print_alphabet_x10(void)
 	char alphabet;
 	int number;
 	number = 0;
-	while ( number <= 10)
+	for(number = 0; number <= 10; number++)
 	{
 		for ( alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		{	_putchar( '%c', alphabet);
+		{	_putchar(alphabet);
 		}
-		number++
-			_putchar('\n');
+	_putchar('\n');
 	}
-	return (0);
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,19 @@
 #include "main.h"
+/**
+ *  print_sign - print the sign of an int
+ *  @n: int to be printed
+ *
+ *  return (0)
+ */
 int print_sign(int n)
 {
-       	if (n > 0)
-	{_putchar( '+');
-	return (1);}
+	if (n > 0)
+	{_putchar('+');
+	return (1); }
 	else if (n == 0)
 	{_putchar('0');
-	return (0);}
+	return (0); }
 	else
 	{_putchar('-');
-	return (-1);}
+	return (-1); }
 }	
-	
