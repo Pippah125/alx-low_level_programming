893341ac2e2ef21c07d1b5939f06953ae932c81b
Pippah Gift <Pippah.gift@gmail.com>
2023-04-05 11:45:36 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,11 @@ void print_alphabet_x10(void)
 {
 	char alphabet;
 	int number;
+
 	number = 0;
-	for(number = 0; number <= 9; number++)
+	for (number = 0; number <= 9; number++)
 	{
-		for ( alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{	_putchar(alphabet);
 		}
 	_putchar('\n');
