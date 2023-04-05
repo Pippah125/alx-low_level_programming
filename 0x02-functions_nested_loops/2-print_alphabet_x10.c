5d73b7b821b792f400918f67ca26ba7ae8a0de3c
Pippah Gift <Pippah.gift@gmail.com>
2023-04-05 11:41:49 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,15 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times in a roe
+ *
+ * return void
+ */
 void print_alphabet_x10(void)
 {
 	char alphabet;
 	int number;
 	number = 0;
-	for(number = 0; number <= 10; number++)
+	for(number = 0; number <= 9; number++)
 	{
 		for ( alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{	_putchar(alphabet);
