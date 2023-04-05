738cda2f6973edd4c32baf39a171ba7252b3ac03
Pippah Gift <Pippah.gift@gmail.com>
2023-04-05 14:14:52 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _abs - prints out absolute value of a number
+ *
+ * @n: number to be printed
+ *
+ * Return: n or -n
+ */
 int _abs(int n)
 {
 	if (("n = 0") && (n > 0))
