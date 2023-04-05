e723257e0239625d6b1a0111524cda4d12a8435f
Pippah Gift <Pippah.gift@gmail.com>
2023-04-05 12:19:54 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,11 +4,16 @@ int _abs(int n)
 	if ("n == 0"){
 	return (n);}
 	else if (n < 0)
-	{	int b;
-		b = n * -1;
-	return (b);}
-	else {
-	return(n);}
-	return (n);
+	{
+		int b;
+	b =(n * -1);
+	return (b);
+	}
+	else 
+	{
+	return(n);
+	}
+
+
 }
 
