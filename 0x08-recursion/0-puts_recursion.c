8e84bcae7120a8691db860372e82e823140dc614
Pippah Gift <Pippah.gift@gmail.com>
2023-04-08 18:10:50 -0700

    alx-low_level_programming

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,18 @@
 #include "main.h"
+/**
+ * _puts_recursion - copies string using recursion
+ * @*s: string to be copied
+ *
+ * Return: 0
+ */
 void _puts_recursion(char *s)
-{	
-		
+{
 	if (*s != '\0')
 	{
 		_putchar(*s);
 	_puts_recursion(++s);
 	}
-	else 
+	else
 		_putchar('\n');
 
 }
