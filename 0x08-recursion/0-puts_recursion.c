a364178da392b2ea3c5434d4acad0b263d7c9efc
Pippah Gift <Pippah.gift@gmail.com>
2023-04-08 18:00:53 -0700

    alx-low_level_programming

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,9 +2,10 @@
 void _puts_recursion(char *s)
 {	
 		
+	if (*s != '\0')
 		_putchar(*s);
-		s++;
-		if (*s != '\0')
-		{	_puts_recursion (*s);
-		}
+	_puts_recursion(s++);
+	else 
+		_putchar('\n');
+
 }
