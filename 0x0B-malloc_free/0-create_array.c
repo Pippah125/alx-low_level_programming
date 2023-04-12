5b4386fdfde4bced16f4dca66ad071bc6328d6ef
Pippah Gift <Pippah.gift@gmail.com>
2023-04-12 05:05:19 -0700

    malloc program

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+char *create_array(unsigned int size, char c)
+{
+	create_array = malloc(sizeof(char) + size of unsigned int);
+		for(i = 0; i <= size; i++)
+		{
+			if size = 0
+			{
+				return(NULL);
+			}
+
+
+			return(NULL);
+		}
+
