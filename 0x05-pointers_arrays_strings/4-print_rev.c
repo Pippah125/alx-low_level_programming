bddf7b5f781bf229e83ce73944eaba244674da88
Pippah Gift <Pippah.gift@gmail.com>
2023-04-16 21:39:14 -0700

    alx

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,12 @@
 void print_rev(char *s)
 {
 	int len;
-	while(s[len] != '\0')
-	{
+	while(s[len] != '\0'){
 		len++;
-	}
 	while(s[len] !='\0')
 	{	len--;
 	_putchar(s[len]);
 	}
-	_putchar('\n');
+	_putchar('\n');}
 	
 }
