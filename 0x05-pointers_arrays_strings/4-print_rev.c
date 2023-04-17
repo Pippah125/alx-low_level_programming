337d98f0f0f27b22de2beeb92f5a4b361b92515c
Pippah Gift <Pippah.gift@gmail.com>
2023-04-16 21:28:38 -0700

    alx

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,14 @@
 #include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: string to be printed
+ *
+ * Return: 0
+ */
 void print_rev(char *s)
 {
 	int len;
 	while(s[len] != '\0')
 		len--;
-	_putchar('s');
+	_putchar("%s", s);
 }
