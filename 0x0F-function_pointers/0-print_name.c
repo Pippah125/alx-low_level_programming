4c48c1228948300008aa4d985e1c8a5b60dc1643
Pippah Gift <Pippah.gift@gmail.com>
2023-04-19 11:38:47 -0700

    alx

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 void print_name(char *name, void (*f)(char *))
 {
-	if (name=='\0')||(f=='\0')
+	if (name == NULL || f == NULL)
 		return ;
-	_putchar(f(name);
+	f(name);
 			
 }
 
