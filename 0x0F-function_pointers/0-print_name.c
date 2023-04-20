abe4653f5064bf32d1c046c60cd47a15b65c1e70
Pippah Gift <Pippah.gift@gmail.com>
2023-04-19 20:54:16 -0700

    alx

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,16 @@
 #include "function_pointers.h"
 #include<stdio.h>
+/**
+ * print_name - function to print out a name 
+ * @name: string to be printed to the command line
+ * @f: function pointer that accepts name as argument
+ *
+ * Return: 0
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if(name == NULL || f == NULL)
 		return ;
-	f(name);
-			
+	f(name);		
 }
 
