71d5ac8dd2806ff7193f3216b2db70a1e078832b
Pippah Gift <Pippah.gift@gmail.com>
2023-04-21 14:15:52 -0700

    alx

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,6 +23,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
                                         printf("%s", separator);
                                 }
 		}
+		  printf("\n");
 	va_end(array);
-	printf("\n");
 }
