3c60f2bb26416a8609b768ff1f154fa23b9c7e40
Pippah Gift <Pippah.gift@gmail.com>
2023-04-21 14:18:40 -0700

    alx

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			x = va_arg(array,char*);
 			if ( x == NULL)
 			{
-				printf("nil");
+				printf("(nil)");
 			}
 			else
 			{
