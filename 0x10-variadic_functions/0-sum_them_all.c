1d9e43d3c333cf02d36dc21eede3324f85ebfa57
Pippah Gift <Pippah.gift@gmail.com>
2023-04-20 21:42:12 -0700

    alx

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,16 +1,25 @@
 #include "variadic_functions.h"
 #include<stdarg.h>
+/** 
+ * sum_them_all - function to sum each of the unsigned int parameters it receives
+ * @n: parameter to be sumed
+ *
+ * Return:0
+ */
 int sum_them_all(const unsigned int n, ...)
-{ 
-	unsigned int a =0;
+{
+	unsigned int a = 0;
+
 	int sum = 0;
+
 	va_list betty;
-	va_start(betty,n);
-	while(a < n)
+	va_start(betty , n);
+
+	while (a < n)
 	{
 	a++;
-		sum += va_arg(betty,unsigned int);
+		sum += va_arg(betty , unsigned int);
 	}
 	 va_end(betty);
-	 return (sum);
+	return (sum);
 }
