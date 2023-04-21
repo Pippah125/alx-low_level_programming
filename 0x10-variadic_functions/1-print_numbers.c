bdf1581c295466859f48670f39ab9eb68ba6f8fd
Pippah Gift <Pippah.gift@gmail.com>
2023-04-21 12:26:58 -0700

    alx

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,10 +9,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		va_start(betty,n);
 		for(d = 0; d < n; d++)
 		{ 
+			 x = va_arg(betty,unsigned int);
+                printf("%d",x);
 			 if ( separator != NULL)
 				 printf("%s",separator);
-		x = va_arg(betty,unsigned int);
-		printf("%d",x);
 		}
 		printf("\n");
 		va_end(betty);
