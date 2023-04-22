a020e9749f40ef69152b636ddbd03f3a00e1365e
Pippah Gift <Pippah.gift@gmail.com>
2023-04-21 19:09:50 -0700

    alx

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include"function_poionters.h"
+#include<stdlib.h>
+int int_index(int *array, int size, int (*cmp)(int))
+{       
+        unsigned int q;
+        if (array == NULL || cmp == NULL)
+        {
+                return (-1);
+        }
+        if(size <= 0)
+        {       
+                return (-1);
+        }       
+	for(q = 0; q < size; q++)
+	{
+		cmp(array[q]);
+		return;
+	}
+}
