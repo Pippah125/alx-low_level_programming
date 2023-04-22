ab62d05d86c4bf6dfbd37c44e3a21a87eb3a0e15
Pippah Gift <Pippah.gift@gmail.com>
2023-04-21 19:39:56 -0700

    alx

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,13 @@
 #include"function_pointers.h"
 #include<stdlib.h>
+/**
+ * int_index - print out integer
+ * @array: pointer to array integer to be printed
+ * @size: size of the array
+ * @cmp: compare the value of these integers
+ *
+ * Return: 0
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {       
          int q;
