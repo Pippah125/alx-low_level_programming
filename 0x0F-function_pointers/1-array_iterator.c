d4db6dde5a5aa57114aeef3bf9207082dd90f4b3
Pippah Gift <Pippah.gift@gmail.com>
2023-04-21 18:48:46 -0700

    alx

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,4 +5,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int  n;
 	for (n = 0; n < size; n++)
 	action(array[n]);
+	if(array == NULL || action == NULL)
+		return;
 }
