1337551d7e2a2cb4ebc051523f393d670bc742b5
Pippah Gift <Pippah.gift@gmail.com>
2023-04-22 21:27:17 -0700

    alx

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef dog_h
 #define dog_h
+/**
+ * struct dog - create a new data type called dog
+ * @name: first member of struct
+ * @owner: second member of struct
+ * @age: third member of struct
+ */
 typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-}dog;
+} dog;
 #endif
