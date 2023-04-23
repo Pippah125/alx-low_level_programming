7ae6717ee74d0c3d933a2d35b9501f33e850be19
Pippah Gift <Pippah.gift@gmail.com>
2023-04-23 08:18:40 -0700

    alx

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,15 @@
 #include"dog.h"
 #include<stdio.h>
 #include<stdlib.h>
+/**
+ * init_dog - defrence the value of a struct to a pointer
+ * @d: pointer to struct dog
+ * @name: points to member name
+ * @age: points to age
+ * @owner: points to member owner
+ *
+ * Return: 0
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
