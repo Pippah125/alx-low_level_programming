f2b89f07b23eb239fae0795a14eef4a8b4be3de7
Pippah Gift <Pippah.gift@gmail.com>
2023-04-24 05:01:17 -0700

    alx

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,16 +4,16 @@
 void print_dog(struct dog *d)
 {
 	if(d == NULL)
-        {
+        
                 return;
-        }
+        
 	if(d->name == NULL)
-	{
+	
 		d->name = "(nil)";
-	}
+	
 	if(d->owner == NULL)
-        {
+        
                 d->owner = "(nil)";
-        }
+        
 		printf("Name:%s\n Age:%f\n Owner:%s\n", d->name, d->age, d->owner);
 }
