e2d9c57001a2cc07fe0aba6dca9fec1ceb21c62d
Pippah Gift <Pippah.gift@gmail.com>
2023-04-25 06:21:53 -0700

    alx

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,10 @@
 #include"main.h"
+/**
+ * _strlen_recursion - return the length of a string
+ * @s:string length to be determined
+ *
+ * Return: 0
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
