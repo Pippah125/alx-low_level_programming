d68fd2b16d2b4351e9c26b2bf44d498525b58782
Pippah Gift <pippah.gift@gmail.com>
2023-06-09 16:56:33 -0700

    alx

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,6 @@
 #include<stdio.h>
-/**
- * main -  print out the name of the program using argv
- * @argc: count the number of elements in argv
- * @argv: write out the number of arguments given
- * Return: 0
- */
-int main(int argc, char *argv[])
+int main(void(int argc), char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
-	return (0);
+	printf("argv[0] = %s\n", argv[0]);
+	return (0); 
 }
