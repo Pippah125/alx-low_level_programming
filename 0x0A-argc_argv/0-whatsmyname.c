74055f0a557c0b20e1937c8bccfd4bd83b0342c0
Pippah Gift <pippah.gift@gmail.com>
2023-06-09 17:20:20 -0700

    alx

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
-int main (__attribute__((unused))int argc, char *argv[])
+/**
+ * main - print argc and argv
+ * @argc: count arguments given to main
+ * @argv: print arguments given to main
+ *
+ */
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (0); 
+	return (0);
 }
