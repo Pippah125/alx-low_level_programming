8f334a999b010589058a236fef2cb414409f2688
Pippah Gift <pippah.gift@gmail.com>
2023-06-14 13:35:41 -0700

    alx

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,15 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * _strdup - creates a copy of a string
+ * @str:string to be copied
+ *
+ * Return: success
+ */
 char *_strdup(char *str)
 {	char *betty;
 	int x;
 	int y;
+
 	y = 0;
 	x = 0;
 
 	if (str == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 
 	while (str[x] != '\0')
@@ -17,18 +24,17 @@ char *_strdup(char *str)
 		x++;
 	}
 
-	betty = malloc(sizeof(char) *(x+1));
+	betty = malloc(sizeof(char) * (x + 1));
 
-	
-	if(betty == NULL)
+	if (betty == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
-	while(str[y])
+	while (str[y])
 	{
 		betty[y] = str[y];
 			y++;
 	}
 	betty[y] = '\0';
-	return(betty);
+	return (betty);
 }
