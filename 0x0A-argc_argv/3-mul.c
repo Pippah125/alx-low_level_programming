aafde136f7d861b7c6cd9653259b97af21cd1dad
Pippah Gift <pippah.gift@gmail.com>
2023-06-15 13:29:38 -0700

    alx

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
+/**
+ * main - add two positive integer
+ * @argc: count argument given
+ * @argv: argument vector
+ *
+ * Return:0
+ */
 int main(__attribute__((unused))int argc, char *argv[])
-{	
+{
 	 int a;
-        int b;
-        int result;
+	int b;
+	int result;
 
-	if(argv[1] == NULL || argv[2] == NULL)
+	if (argv[1] == NULL || argv[2] == NULL)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
 
 	a = atoi(argv[1]);
-	b= atoi(argv[2]);
+	b = atoi(argv[2]);
 	result = (a * b);
 
-	printf("%d\n",result);
-		return(0);
+	printf("%d\n", result);
+		return (0);
 }
