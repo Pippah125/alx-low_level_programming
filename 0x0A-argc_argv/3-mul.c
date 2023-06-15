951412cdfd7ee3e7171de71daa3b6bc8796c150c
Pippah Gift <pippah.gift@gmail.com>
2023-06-15 13:20:37 -0700

    alx

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(__attribute__((unused))int argc, char *argv[])
-{
-	int a;
-	int b;
-	int result;
+{	
+	 int a;
+        int b;
+        int result;
+
+	if(argv[1] == NULL || argv[2] == NULL)
+	{
+		printf("Error\n");
+		return(1);
+	}
 
 	a = atoi(argv[1]);
 	b= atoi(argv[2]);
 	result = (a * b);
 
-	if (argv[1] == NULL)
-	{
-		printf("Error\n");
-		return(1);
-	}
-	if (argv[2] == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
 	printf("%d\n",result);
 		return(0);
 }
