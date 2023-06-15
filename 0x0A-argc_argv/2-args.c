1fe6dd8b17d4135329e3b65529d7955ae3bb2b69
Pippah Gift <pippah.gift@gmail.com>
2023-06-15 12:35:08 -0700

    alx

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
+/**
+ * main- prints all arguments received
+ * @argc:count all arguments received
+ * @argv:agrument vector
+ *
+ * Return:0
+ */
 int main(__attribute__((unused))int argc, char *argv[])
 {
 	int i;
+
 	i = 0;
-	while(argv[i] != NULL)
+
+	while (argv[i] != NULL)
 	{
-		printf("%s\n",argv[i]);
+		printf("%s\n", argv[i]);
 			i++;
 	}
-	return(0);
+	return (0);
 }
 
