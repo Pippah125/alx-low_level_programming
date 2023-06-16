db36186fe8d1acddfb3aa4ca5c81a44b88a72991
Pippah Gift <pippah.gift@gmail.com>
2023-06-15 18:12:27 -0700

    alx

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,18 +1,25 @@
 #include"main.h"
 #include<stdlib.h>
 #include<string.h>
+/**
+ * _calloc - allocates space for nmemb
+ * @nmemb: elements
+ * @size:size of elements
+ *
+ * Return:0
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *betty;
 		if (nmemb == 0 || size == 0)
 		{
-			return(NULL);
+			return (NULL);
 		}
 		betty = malloc(nmemb * size);
 	if (betty == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
-	memset(betty,0, nmemb *size);
-	return(betty);
+	memset(betty, 0, nmemb * size);
+	return (betty);
 }
