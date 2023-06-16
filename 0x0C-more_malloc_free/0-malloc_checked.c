908b247e25790178107f37fcd754edd8fd943c39
Pippah Gift <pippah.gift@gmail.com>
2023-06-15 18:03:58 -0700

    alx

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,20 @@
 #include<stdlib.h>
 #include"main.h"
+/**
+ *malloc_checked - allocates space for array
+ * @b:size of memory to be allocated
+ *
+ * Return: 0
+ */
 void *malloc_checked(unsigned int b)
 {
-	void* betty;
+	void *betty;
+
 	betty = malloc((b));
-	if(betty == NULL)
+
+	if (betty == NULL)
 	{
 		exit(98);
 	}
-	return(betty);
+	return (betty);
 }
