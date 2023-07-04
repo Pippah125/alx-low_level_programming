07d227ae1662f9482ae2768997a2d77a558deb0f
Pippah125 <Pippah.gift@gmail.com>
2023-07-04 02:09:28 +0000

    alx

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"lists.h"
-struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
 size_t print_list(const list_t *h)
 {
 	typedef struct list_s
@@ -15,19 +9,23 @@ size_t print_list(const list_t *h)
 		unsigned int len;
 		struct list_s *next;
 	} list_t;
-	 list_t *head = NULL
-		head = malloc(sizeof(list_t));
-	 int count;
-	 if( str == NULL);
-	 printf("[0] (nil)");
-	 else
-	 {
-		 list_t betty = NULL;
-		 betty = head;
-		 for(count = 0; betty != NULL; count++);
-		 {
-			 betty = betty -> next;
-		 }
-	 }
+	size_t count;
+	list_t *head;
+	head = (list_t *)h;
+	count = 0;
+	while(head != NULL)
+	{
+
+	 	if( head ->str == NULL)
+	 	{
+	 	printf("[%lu] (nil)\n",count);
+	 	}
+	 	else
+	 	{
+		 printf("[%lu] %s\n", count, head -> str);
+	 	}
+		count++;
+		head  = head ->next;
+	}
 	 return(count);
 }
