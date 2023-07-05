1fec1d1f59bbfabf7bcd7ca99b0d06f6ff800bad
Pippah125 <Pippah.gift@gmail.com>
2023-07-05 10:40:20 +0000

    alx

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * _pow_recursion-returns value of x raised to power y
+ * @x: raised to power y
+ * @y: number
+ *
+ * Return:0
+ */
+int -pow_recursion(int x, int y)
+{
+	if(y<0)
+	{
+		return(-1);
+	}
+	if(y==0)
+	{
+		return(1);
+	}
+	return(x * _pow_recursion(x,y -1));
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include"main.h"
+#include<stdio.h>
+/** betty - calculate square of number
+ * @n: square root to calculate
+ * @i:loop through number
+ *
+ * Return: the square root
+ */
+int betty (int n, int z)
+{
+	int square = z * z;
+	if(square > n)
+	{
+		return(-1);
+	}
+	if (square  == n)
+	{
+		return(z);
+	}
+        return(square(n, z+1));
+}		              
+
+/**
+ * _sqrt_recursion -return perfect square root of a number
+ * @n: number to calculate the square root
+ *
+ * Return: the perfect square root
+ */
+int _sqrt_recursion(int n)
+{
+	return(betty(n,1));
+}
+
