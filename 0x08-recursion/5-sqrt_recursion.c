f99ab68f1ea546fbacaaa84ce1d235b0885d2dc7
Pippah125 <Pippah.gift@gmail.com>
2023-07-05 11:17:10 +0000

    alx

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,24 +1,25 @@
 #include"main.h"
 #include<stdio.h>
-/** betty - calculate square of number
+/** 
+ * betty - calculate square of number
  * @n: square root to calculate
  * @i:loop through number
  *
  * Return: the square root
  */
-int betty (int n, int z)
+int betty(int n, int z)
 {
 	int square = z * z;
-	if(square > n)
+	if (square > n)
 	{
-		return(-1);
+		return (-1);
 	}
 	if (square  == n)
 	{
-		return(z);
+		return (z);
 	}
-        return(betty(n, z+1));
-}		              
+	return (betty(n, z + 1));
+}    
 
 /**
  * _sqrt_recursion -return perfect square root of a number
@@ -28,6 +29,6 @@ int betty (int n, int z)
  */
 int _sqrt_recursion(int n)
 {
-	return(betty(n,1));
+	return (betty(n , 1));
 }
 
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,25 +3,25 @@
 /**
  * betty - check if number is a prime number
  * @n: number to check
- * @i:loop through number
+ * @a:loop through number
  *
- * Retrun:0
+ * Retrun: 0
  */
 int betty(int n, int a)
 {
-	if(n <= 1)
+	if (n <= 1)
 	{
-		return(0);
+		return (0);
 	}
-	if(n % a == 0 && a >1)
+	if (n % a == 0 && a > 1)
 	{
-		return(0);
+		return (0);
 	}
-	if((n/a) < a)
+	if ((n / a) < a)
 	{
-		return(1);
+		return (1);
 	}
-	return (betty(n, a +1));
+	return (betty(n , a + 1));
 }
 /**
  * is_prime_number - checks if a number is prime
@@ -31,5 +31,5 @@ int betty(int n, int a)
  */
 int is_prime_number(int n)
 {
-	return(betty(n,1));
+	return (betty(n , 1));
 }
