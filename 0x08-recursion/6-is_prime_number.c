25d897df847cd283c9994a1ee2b3b919d9d5c8e1
Pippah125 <Pippah.gift@gmail.com>
2023-07-05 10:57:41 +0000

    alx

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * betty - check if number is a prime number
+ * @n: number to check
+ * @i:loop through number
+ *
+ * Retrun:0
+ */
+int betty(int n, int a)
+{
+	if(n <= 1)
+	{
+		return(0);
+	}
+	if(n % a == 0 && a >1)
+	{
+		return(0);
+	}
+	if((n/a) < a)
+	{
+		return(1);
+	}
+	return (betty(n, a +1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: number to be checked
+ *
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+	return(betty(n,1));
+}
