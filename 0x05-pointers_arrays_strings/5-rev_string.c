0df1232320d56a3a8258c29378ccf46220b78dec
Pippah <Pippah.gift@gmail.com>
2023-08-12 00:32:13 -0700

    alx

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,12 +12,10 @@ void rev_string(char *s)
 
 	for (betty = 0; s[betty] != '\0'; betty++)
 	{
-		_putchar(s[betty]);
 	}
 	for(i = betty - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
-	_putchar('\n');
 
 }
