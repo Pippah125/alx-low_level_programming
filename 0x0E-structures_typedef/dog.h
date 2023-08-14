e7b1a7339e4d97e77ed2fd5bf9faa29d5de264ad
Pippah <Pippah.gift@gmail.com>
2023-08-14 09:53:59 -0700

    alx

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - defines a new data type called dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
