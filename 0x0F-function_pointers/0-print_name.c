911dcc679d1d8b12f6bc2a5e66110501e980734b
Pippah <Pippah.gift@gmail.com>
2023-08-16 06:11:03 -0700

    alx

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,13 @@
 #include<stdlib.h>
 #include"function_pointers.h"
+/**
+ * print_name - print name if name is not null
+ * @name:character to be printed
+ * @f:function that points to a function that prints
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if(name == NULL || f == NULL)
+	if (name == NULL || f == NULL)
 	{
 		return;
 	}
