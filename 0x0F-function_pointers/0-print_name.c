80f1bababcc3fb2bdeeb50e9dc554ea441e61dc1
Pippah <Pippah.gift@gmail.com>
2023-08-16 06:07:40 -0700

    alx

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,5 @@
+#include<stdlib.h>
 #include"function_pointers.h"
-#include <stddef.h>
-void g(char *s);
-void g(char *s)
-{
-	int i;
-	for(i = 0; s[i] != '\0'; i++)
-	{
-		_putchar(s[i]);
-	}
-}
-void (*f)(char *) = &g;
 void print_name(char *name, void (*f)(char *))
 {
 	if(name == NULL || f == NULL)
