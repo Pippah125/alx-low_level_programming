c1c4b3ede789efac0ecd7cd3bf1e675a710e9ce3
Pippah125 <Pippah.gift@gmail.com>
2023-08-27 09:42:59 +0000

    alx

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,17 +1,59 @@
 #include<stdlib.h>
 #include"dog.h"
+int strlen2(char *s);
+char *copy(char *dest, char *src);
+int strlen2(char *s)
+{
+	int i;
+	for(i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return(i);
+}
+int *copy(char *dest, char *src)
+{
+	int i;
+	for(i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for(;dest[i] != '\0'; i++)
+	{
+	}
+	return(dest);
+}
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int new_name;
+	int new_owner;
+	new_name = 0;
+	new_owner = 0;
 	dog_t *my_dog;
-	my_dog = malloc(sizeof(dog_t));
-	if (my_dog == NULL)
+	if(name != NULL && owner != NULL)
 	{
-		return(NULL);
-	}
-	else
-	{
-		my_dog -> name = name;
-		my_dog -> owner = owner;
+		new_name = strlen2(name) +1;
+		new_owner = strlen2(owner) + 1;
+		my_dog = malloc(sizeof(dog_t));
+		if (my_dog == NULL)
+		{
+			return(NULL);
+		}
+		my_dog->name = malloc(sizeof(char) * new_name);
+		if(my_dog ->name == NULL)
+		{
+			free(my_dog);
+			return(NULL);
+		}
+		my_dog->owner = malloc(sizeof(char) * new_owner);
+			if(my_dog->owner == NULL)
+			{
+				free(my_dog->name);
+				free(my_dog);
+				return(NULL);
+			}
+			my_dog->name = copy(my_dog->name,name);
+			my_dog->owner = copy(my_dog->owner,owner);
+			my_dog->age = age;
 	}
 	return(my_dog);
 }
