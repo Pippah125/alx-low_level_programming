464138dcf9cbab95fcea781a12f4b81c395345f5
Pippah125 <Pippah.gift@gmail.com>
2023-08-29 15:36:27 +0000

    alx

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,39 +1,38 @@
-#include"main.h"
-#include<stdlib.h>
+#include "main.h"
+#include <stdlib.h>
+
 int **alloc_grid(int width, int height)
 {
 	int **mem;
-	int i, j, k, l;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 	{
-		return (NULL);
-	}
-	mem = (int **)malloc(sizeof(int *) * width);
-	if (mem == NULL)
-	{
-		free(mem);
 		return NULL;
 	}
-	for (i = 0; i < width; i++)
+	mem = (int **)malloc(sizeof(int *) * height);
+	if (mem == NULL)
 	{
-		mem[i] = (int *)malloc(sizeof(int) * height);
+		return NULL;
+	}
+	for (i = 0; i < height; i++)
+	{
+		mem[i] = (int *)malloc(sizeof(int) * width);
 		if (mem[i] == NULL)
 		{
-			for (j = i; j >= 0; j--)
+			for (j = i - 1; j >= 0; j--)
 			{
 				free(mem[j]);
 			}
 			free(mem);
 			return NULL;
 		}
-		for (k = 0; k < height; k++)
-		{
-			for(l = 0; l < width; l++)
-			{
-				mem[k][l] = 0;
-			}
-		}
-	}						    
-				return (mem);
+	for (j = 0; j < width; j++)
+	{
+		mem[i][j] = 0;
+	}
+	}
+
+	return mem;
 }
+
