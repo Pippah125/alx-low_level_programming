8dfaa762ad90aa3e003d5e98477905c06c4d1c99
Pippah125 <Pippah.gift@gmail.com>
2023-08-30 23:55:42 +0000

    alx

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include"main.h"
+#include<stdlib.h>
+#include<string.h>
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int k, sum = 0;
+	char * betty;
+	if(argc >1)
+	{
+		for(i = 1; i < argc; i++)
+		{
+			e = argv[i];
+			for(k = 0; k < strlen(betty); k++)
+			{
+				if(betty[k] < 48 || betty[k] > 57)
+				{
+					printf("Error\n");
+					return(1);
+				}
+			}
+			sum = sum + atoi(betty);
+			betty++;
+		}
+		printf("%d\n",sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return(0);
+}
