750ea9750f7149426649872791d0e4dd54830536
Pippah125 <Pippah.gift@gmail.com>
2023-08-31 00:06:31 +0000

    alx

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,32 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+/**
+ * main-takes any arguments and adds them up
+ * @argc:argument count
+ * @argv:argument vector
+ *
+ * Return:0
+ */
 int main(int argc, char *argv[])
 {
 	int i;
 	unsigned int k, sum = 0;
-	char * betty;
-	if(argc >1)
+	char *betty;
+
+	if (argc > 1)
 	{
-		for(i = 1; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
 			betty = argv[i];
-			for(k = 0; k < strlen(betty); k++)
+			for (k = 0; k < strlen(betty); k++)
 			{
-				if(betty[k] < 48 || betty[k] > 57)
+				if (betty[k] < 48 || betty[k] > 57)
 				{
 					printf("Error\n");
-					return(1);
+					return (1);
 				}
 			}
 			sum = sum + atoi(betty);
 			betty++;
 		}
-		printf("%d\n",sum);
+		printf("%d\n", sum);
 	}
 	else
 	{
 		printf("0\n");
 	}
-	return(0);
+	return (0);
 }
