39455cfd760b92daaaf7863bbffc912adf48d51d
Pippah125 <Pippah.gift@gmail.com>
2023-09-29 01:01:46 +0000

    alx

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _strlen_recursion- returns the length of a string
+ * @s: string to be counted
+ *
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+	i = 0;
+	if (*s != '\0')
+	{
+		i = i + 1;
+		i = i + _strlen_recursion(s + 1);
+	}
+	return (i);
+}
