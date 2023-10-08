f4ec71f230bd6cf451dd800234b5de992a9cf0ce
Pippah125 <Pippah.gift@gmail.com>
2023-10-08 11:56:39 +0000

    alx

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		length = A + n;
 	}
-	betty = malloc(sizeof(char) * length);
+	betty = malloc(sizeof(char) * length + 1);
 	if(betty == NULL)
 	{
 		return (NULL);
