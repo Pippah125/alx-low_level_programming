6bb3415715204f0f817450c3bca092de89def0c9
Pippah125 <Pippah.gift@gmail.com>
2023-10-11 13:13:26 +0000

    alx

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include<stdlib.h>
 #include"dog.h"
+#include<stdio.h>
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this project is on understanding structures and pointers to structures
