c3bfca0e3c34fa3276381cd4ae555fee65a69eef
Pippah125 <Pippah.gift@gmail.com>
2023-10-11 13:29:36 +0000

    alx

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,8 @@
+#ifndef PI_H
+#define PI_H
+/**
+ * File: contains pi
+ * desc: size of pi
+ */
+#define PI 3.14159265359
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef A_B_S
+#define A_B_S
+#define ABS ((x) < 0 ? ((x) * -1) : (x))
+#endif
