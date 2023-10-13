eebcbab4158a477be8d06c642a10dc0172641b1d
Pippah125 <Pippah.gift@gmail.com>
2023-10-13 23:05:18 +0000

    alx

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,7 +2,7 @@
 #include<stdarg.h>
 /**
  * sum_them_all - calculates the addition of its parameters
- * @n: parameters passed to function 
+ * @n: parameters passed to function
  *
  * Return: 0
  */
@@ -11,8 +11,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_list numbers;
 	unsigned int count;
 	unsigned int result;
-	va_start(numbers,n);
-	for(count = 0; count < n; count++)
+
+	va_start(numbers, n);
+	for (count = 0; count < n; count++)
 	{
 		result = result + va_arg(numbers, int);
 	}
