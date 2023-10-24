5dd822a6035d8e4e1a83b6054f09d89162680783
Pippah Gift <Pippah.gift@gmail.com>
2023-10-24 05:10:20 -0700

    alx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,47 +11,53 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node;
-	listint_t *copy;
-	unsigned int count;
-	if(head!= NULL)
-	{
-		new_node = malloc(sizeof(listint_t));
-	
-	if(new_node == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		new_node->n =n;
-	}
-	if(idx > 0)
-	{
-		copy = *head;
-	
-		while(copy != NULL)
-		{
-		if(count == idx )
-		{
-			new_node->next =copy->next;
-			copy->next = new_node;
-			return (new_node);
-		}
-		copy = copy->next;
-		count++;
-		}
-		if(idx > count)
-		{
-			return(NULL);
-		}
-	}
-	else
-	{
-		new_node->next =*head;
-		*head =new_node;
-	}
-	return(new_node);
+    listint_t *new_node;
+    listint_t *copy;
+    unsigned int count = 0;
+
+    if (head != NULL)
+    {
+        new_node = malloc(sizeof(listint_t));
+
+        if (new_node == NULL)
+        {
+            return (NULL);
+        }
+        else
+        {
+            new_node->n = n;
+        }
+
+        if (idx > 0)
+        {
+            copy = *head;
+
+            while (copy != NULL)
+            {
+                if (count == idx - 1)
+                {
+                    new_node->next = copy->next;
+                    copy->next = new_node;
+                    return (new_node);
+                }
+                copy = copy->next;
+                count++;
+            }
+
+            if (idx > count)
+            {
+                free(new_node);
+               return (NULL);
+            }
+        }
+        else
+        {
+            new_node->next = *head;
+            *head = new_node;
+            return (new_node);
+        }
+    }
+
+    return (NULL);
 }
-	return (NULL);
-}
+
