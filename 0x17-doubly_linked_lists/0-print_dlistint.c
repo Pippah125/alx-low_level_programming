a918a105d6133bbfa6f890d0101b3bdcc6ac2b07
Pippah Gift <Pippah.gift@gmail.com>
2023-12-13 16:51:57 -0800

    alx

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,12 @@
 #include"lists.h"
 #include<stdio.h>
 #include<stdlib.h>
+/**
+ * print_dlistint - print and count elements and nodes
+ * @h:struct to dislint
+ *
+ * Return:0
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
