1ee4d2391c9598114e24c5f1194ac67561602dd1
Pippah Gift <Pippah.gift@gmail.com>
2023-12-13 23:25:22 -0800

    alx

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,15 +1,21 @@
 #include"lists.h"
 #include<stdio.h>
+/**
+ * dlistint_len- returns lenght of elements in lists
+ * @h:list
+ *
+ * Return:0
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t count;
 
 	count = 0;
-	while(current != NULL)
+	while (current != NULL)
 	{
 		count++;
 		current = current->next;
 	}
-	return(count);
+	return (count);
 }
--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,13 @@
 #include"lists.h"
 #include<stdio.h>
 #include<stdlib.h>
+/**
+ * add_dnodeint- adds nodes to the begining of linked list
+ * @head:list
+ * @n:data
+ *
+ * Return:0
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
